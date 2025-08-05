0b93993bb3f3cc786b13420202d3efeb5db31ca4
이은정 <ej2grateful@gmail.com>
2025-08-05 10:37:55 +0900

    LoginForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Article from "./components/Article";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   return (
-    <div className="bg-cyan-100 min-h-screen flex justify-center items-center">
+    <div className="bg-red-100 min-h-screen flex justify-center items-center yang-jin">
       <Article />
+      <LoginForm />
     </div>
   );
 };
