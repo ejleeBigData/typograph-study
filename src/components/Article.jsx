601d7e43b3076d0483695fcc7f1c9d01eaa3fdb8
이은정 <ej2grateful@gmail.com>
2025-08-05 10:34:24 +0900

    first commit

---
+++ src/components/Article.jsx
@@ -0,0 +1,17 @@
+const Article = () => {
+  return (
+    <article className="prose prose-neutral prose-xl max-w-2xl mx-auto p-6">
+      <h2>타이포그래피 플러그인으로 문서만들기 </h2>
+      <p>
+        타이포그래피 플러그인을 사용하여 문서를 작성하는 것은 매우 간단합니다.
+      </p>
+      <ul>
+        <li>자동 마진 및 패딩</li>
+        <li>제목, 본문, 인용구 스타일</li>
+        <li>반응형 폰트 크기</li>
+      </ul>
+    </article>
+  );
+};
+
+export default Article;
